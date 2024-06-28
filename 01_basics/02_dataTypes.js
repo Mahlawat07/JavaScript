2d0fa3fd0e468a55134d285dcd1b1583cc44ba0e
Mahlawat07 <92742799+Mahlawat07@users.noreply.github.com>
2024-06-28 08:09:32 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert ( 3 + 3 ) // we are using nodejs, not browser
+
+
+console.log( 3 
+    + 3) // code readability shoule be high
+
+console.log("Sagar")
+
+let name = "sagar"
+let age = 18
+let isLoggedIn = false 
+let state;
+
+
+// number = 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone variable
+// undefined =>
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object 
