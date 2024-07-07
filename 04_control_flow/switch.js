2419f9eeb92ea92ba8cb78d3f9a9e3e27704fefe
Mahlawat07 <92742799+Mahlawat07@users.noreply.github.com>
2024-07-07 11:34:24 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,31 @@
+// syntax
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = "March"
+
+switch (month) {
+    case "January":
+        console.log("January");        
+        break;
+    case "February":
+        console.log("February");        
+        break;
+    case "March":
+        console.log("March");        
+        break;
+    case "April":
+        console.log("April");        
+        break;
+
+    default:
+        console.log("default case is match")
+        break;
+}
